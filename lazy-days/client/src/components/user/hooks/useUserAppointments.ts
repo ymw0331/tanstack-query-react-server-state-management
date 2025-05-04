6447afaac82bae3a09383c250618318edccbad7e
Wayne Yong <ymw0331@gmail.com>
2025-05-04 16:12:33 +0800

    fetch user appointments

--- lazy-days/client/src/components/user/hooks/useUserAppointments.ts
+++ lazy-days/client/src/components/user/hooks/useUserAppointments.ts
@@ -1,21 +1,36 @@
-import type { Appointment } from "@shared/types";
 
 import { axiosInstance, getJWTHeader } from "../../../axiosInstance";
 
 import { useLoginData } from "@/auth/AuthContext";
 
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { generateUserAppointmentsKey } from "@/react-query/key-factories";
+import { Appointment } from "@shared/types";
+
 // for when we need a query function for useQuery
-// async function getUserAppointments(
-//   userId: number,
-//   userToken: string
-// ): Promise<Appointment[] | null> {
-//   const { data } = await axiosInstance.get(`/user/${userId}/appointments`, {
-//     headers: getJWTHeader(userToken),
-//   });
-//   return data.appointments;
-// }
+async function getUserAppointments(
+  userId: number,
+  userToken: string
+): Promise<Appointment[] | null> {
+  const { data } = await axiosInstance.get(`/user/${userId}/appointments`, {
+    headers: getJWTHeader(userToken),
+  });
+  return data.appointments;
+}
 
 export function useUserAppointments(): Appointment[] {
-  // TODO replace with React Query
-  return [];
+
+  const { userId, userToken } = useLoginData()
+
+  const fallback: Appointment[] = []
+
+  const { data: userAppointments = fallback } = useQuery({
+    enabled: !!userId, // return only userId is truthy
+    queryKey: generateUserAppointmentsKey(userId, userToken),
+    queryFn: () => getUserAppointments(userId, userToken),
+    staleTime: Infinity,
+  })
+
+
+  return userAppointments;
 }
--- lazy-days/client/src/react-query/key-factories.ts
+++ lazy-days/client/src/react-query/key-factories.ts
@@ -3,3 +3,7 @@ import { queryKeys } from "./constants"
 export const generateUserKey = (userId: number, userToken: string) => {
     return [queryKeys.user, userId, userToken]
 }
+
+export const generateUserAppointmentsKey = (userId: number, userToken: string) => {
+    return [queryKeys.appointments, userId, userToken]
+}
